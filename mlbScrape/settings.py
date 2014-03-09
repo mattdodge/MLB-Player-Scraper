9bfd339724188992ddfb97780cb6d51f01c4bf8f
Matt Dodge <mattedgod@gmail.com>
2014-03-08 17:24:49 -0800

    Initial Scrapy Config

---
+++ mlbScrape/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for mlbScrape project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'mlbScrape'
+
+SPIDER_MODULES = ['mlbScrape.spiders']
+NEWSPIDER_MODULE = 'mlbScrape.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'mlbScrape (+http://www.yourdomain.com)'
